d662b7a2f28424128ef7e3245598f44d8cc13c08
chiragw <chiragwadhwa369@gmail.com>
2019-10-02 12:51:17 +0530

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish{
 	id: string;
 	name: string;
@@ -7,4 +9,5 @@ export class Dish{
 	price: string;
 	description: string;
 	label: string;
+	comments: Comment[];
 }
